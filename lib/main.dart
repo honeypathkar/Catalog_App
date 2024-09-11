ce5aa427ac1c4f8be59b3a39b7591fd0bd441183
honeypathkar <honeypatkar70@gmail.com>
2024-09-11 14:03:41 +0530

    Adding AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,35 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Catalog App'),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-            // ignore: avoid_unnecessary_containers
-            child: Container(child: const Text("Welcome"))));
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+          child: const Text("Welcome to Catalog App"),
+        ),
+      ),
+      endDrawer: const Drawer(),
+    );
+  }
+}
