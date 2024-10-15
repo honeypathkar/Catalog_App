6d7bd6d97b8352f7f4ca819b2ecaa8a4c68fb7b9
honeypathkar <honeypatkar70@gmail.com>
2024-10-15 13:19:31 +0530

    Using bedge to show cart count

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shopping_cart/core/store.dart';
+import 'package:shopping_cart/models/cart.dart';
 import 'dart:convert';
 import 'package:shopping_cart/models/catalog.dart';
 import 'package:shopping_cart/pages/home_widget/catalog_header.dart';
@@ -7,6 +9,7 @@ import 'package:shopping_cart/pages/home_widget/catalog_list.dart';
 import 'package:shopping_cart/utils/myroute.dart';
 import 'package:shopping_cart/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
+// import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +20,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // final url = "https://fakestoreapi.com/products";
   @override
   void initState() {
     super.initState();
@@ -27,6 +31,8 @@ class _HomePageState extends State<HomePage> {
     await Future.delayed(const Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
+    /*  final response = await http.get(Uri.parse(url));
+    final catalogJson = response.body; */
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModels.items = List.from(productsData)
@@ -37,16 +43,32 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable, no_leading_underscores_for_local_identifiers
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
       backgroundColor: MyTheme.creamColor,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, MyRoute.cartRoute),
-        backgroundColor: MyTheme.darkBluishColor,
-        shape: const CircleBorder(),
-        child: const Icon(
-          Icons.shopping_cart_outlined,
-          color: Colors.white,
-        ),
+      floatingActionButton: VxBuilder(
+        mutations: const {AddMutation, RemoveMutation},
+        builder: (context, _, __) {
+          return FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoute.cartRoute),
+            backgroundColor: MyTheme.darkBluishColor,
+            shape: const CircleBorder(),
+            child: const Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.white,
+            ),
+          ).badge(
+            color: Vx.gray200,
+            size: 22,
+            count:
+                _cart.items.length, // Ensure _cart is properly defined and used
+            textStyle: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          );
+        },
       ),
       body: SafeArea(
         child: Container(
